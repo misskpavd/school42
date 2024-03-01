16287c0923f078ad900110356b60d7ecb3c15848
misskpavd <147539950+misskpavd@users.noreply.github.com>
2024-03-01 18:27:19 +0100

    Add files via upload

--- 01_libft/ft_split.c
+++ 01_libft/ft_split.c
@@ -6,37 +6,79 @@
 /*   By: kvan-don <kvan-don@student.42luxembourg.l  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 11:57:08 by kvan-don          #+#    #+#             */
-/*   Updated: 2024/03/01 12:02:17 by kvan-don         ###   ########.fr       */
+/*   Updated: 2024/03/01 18:26:41 by kvan-don         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Allocates (with malloc(3)) and returns an array of strings obtained by
 splitting ’s’ using the character ’c’ as a delimiter. The array must end
-with a NULL pointer. */
+with a NULL pointer. 
+You need to examine the string s and cut it into pieces every time you encounter
+the delimiter character c.*/
 
-int word_count(/* whatever parameter you need */)
+#include "libft.h"
+
+static int	count_words(char const *s, char c)
 {
-   /* find and return the number of words in the string */
+	unsigned int	i;
+	int				count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			count++;
+		while (s[i] && (s[i] != c))
+			i++;
+	}
+	return (count);
 }
 
-void ft_free(/* whatever argument you need */)
+static char	*ft_strndup(const char *s, size_t n)
 {
-   /* free EVERYTHING you allocated */
-   /* each element of the array as well as the array */
-}
+	char	*str;
+	size_t	i;
 
-char *fill_word(/* whatever argument you need */)
-{
-   /* allocate enough room for the word */
-   /* copy the word into the memory you allocated above */
-   /* return the allocated word */
+	i = 0;
+	str = (char *)malloc(sizeof(char) * n + 1);
+	if (str == NULL)
+		return (NULL);
+	while (i < n && s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
 }
 
 char	**ft_split(char const *s, char c)
 {
-   /* allocate an array big enough to hold all the words in s */
-   /* loop over the string and find the start of the word */
-      /* find the end of the word */
-         /* copy the world at the first free index in our words array */
-   /* return our words array */ 
+	int		i;
+	int		j;
+	int		k;
+	char	**tab;
+
+	i = 0;
+	k = 0;
+	tab = (char **)malloc(sizeof(char *) * (count_words(s, c)) + 1);
+	if (tab == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		j = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > j)
+		{
+			tab[k] = ft_strndup(s + j, i - j);
+			k++;
+		}
+	}
+	tab[k] = NULL;
+	return (tab);
 }
