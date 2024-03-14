119cf630204819593631eef95fcda186422af90f
misskpavd <misskpavd@github.com>
2024-03-14 11:06:26 +0100

    Start of my prinft function

---
+++ printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvan-don <kvan-don@student.42luxembourg.l  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 11:03:59 by kvan-don          #+#    #+#             */
+/*   Updated: 2024/03/14 11:05:52 by kvan-don         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
