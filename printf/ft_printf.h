5da24e49e1a17654309674217091d7b23a364d19
misskpavd <misskpavd@github.com>
2024-03-14 12:09:42 +0100

    main prinft function and header are finished, until testing reveals errors

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kvan-don <kvan-don@student.42luxembourg.l  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 11:03:59 by kvan-don          #+#    #+#             */
-/*   Updated: 2024/03/14 11:05:52 by kvan-don         ###   ########.fr       */
+/*   Updated: 2024/03/14 12:08:32 by kvan-don         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+# define LOW_CASE_HEX "0123456789abcdef"
+# define UP_CASE_HEX "0123456789ABCDEF"
+
 int	ft_printf(const char *str, ...);
+void	ft_putchar_pf(char c, size_t *count);
+void	ft_putstr_pf(char *str, size_t *count);
+void    ft_putptr_pf(void *ptr, size_t *count);
+void	ft_putnbr_pf(int num, size_t *count);
+void	ft_putuint_pf(unsigned int num, size_t *count);
+void	ft_puthex_pf(unsigned int num, size_t *count, char *base);
 
 #endif
---
+++ printf/sources/ft_printf.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvan-don <kvan-don@student.42luxembourg.l  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 11:02:56 by kvan-don          #+#    #+#             */
+/*   Updated: 2024/03/14 11:38:05 by kvan-don         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_format(va_list var, char *str, size_t count)
+{
+	if (*str == 'c')
+		ft_putchar_pf(va_arg(var, int) count);
+	else if (*str == 's')
+		ft_putstr_pf(va_arg(var, char*) count);
+	else if (*str == 'p')
+		ft_putptr_pf(va_arg(var, void *) count);
+	else if (*str == 'i' || *str == 'd')
+		ft_putnbr_pf(va_arg(var, int) count);
+	else if (*str == 'u')
+		ft_putuint_pf(va_arg(var, unsigned int) count);
+	else if (*str == 'x')
+		ft_puthex_pf(va_arg(var, unsigned int) count, LOW_CASE_HEX);
+	else if (*str == 'X')
+		ft_puthex_pf(va_arg(var, unsigned int) count, UP_CASE_HEX);
+	else if (*str == '%')
+		ft_putchar_pf(*str, count);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_end	var;
+	size_t	count;
+
+	if (!str)
+		return (0);
+	count = 0;
+	va_start(var, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			ft_format(va, (char *)str, &counter);
+		}
+		else
+			ft_putchar_pf(*str, &counter);
+		str++;
+	}
+	va_end(var);
+	return (count);
+}
